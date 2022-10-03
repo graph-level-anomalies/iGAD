fa0ec5848d0de9f00173458ca365ac99b7f42ae2
graph-level-anomalies <114328513+graph-level-anomalies@users.noreply.github.com>
2022-10-03 11:16:25 +1100

    Update model_topo_.py

--- model_topo_.py
+++ model_topo_.py
@@ -13,20 +13,14 @@ class RW_GNN(nn.Module):
         self.max_step = max_step
         self.device = device
         self.normalize = normalize
-
-
         self.Theta_matrix = Parameter(
-            torch.FloatTensor(self.n_subgraphs, self.size_subgraphs * (self.size_subgraphs - 1) // 2, 1))  # random walk
-
-
+            torch.FloatTensor(self.n_subgraphs, self.size_subgraphs * (self.size_subgraphs - 1) // 2, 1))
         self.bn = nn.BatchNorm1d(self.n_subgraphs * (self.max_step-1))
         self.dropout = nn.Dropout(p=dropout)
         self.init_weights()
-
         self.layer_1 = nn.Linear(self.n_subgraphs*(self.max_step-1), self.hidden_dim, bias= True)
         self.layer_2 = nn.Linear(self.hidden_dim, self.output_dim, bias=True)
         self.relu = nn.ReLU()
-
 
     def init_weights(self):
         #self.Theta_matrix.data.uniform_(-1, 1)
@@ -35,20 +29,16 @@ class RW_GNN(nn.Module):
     def forward(self, adj, graph_indicator):
         sampled_matrix = self.relu(self.Theta_matrix)
         sampled_matrix = sampled_matrix[:, :, 0]
-
-        # transform
         adj_sampled = torch.zeros(self.n_subgraphs, self.size_subgraphs, self.size_subgraphs).to(self.device)
         idx = torch.triu_indices(self.size_subgraphs, self.size_subgraphs, offset=1)
         adj_sampled[:, idx[0], idx[1]] = sampled_matrix
         adj_sampled = adj_sampled + torch.transpose(adj_sampled, 1, 2)
-
-        # random walks
         unique, counts = torch.unique(graph_indicator, return_counts=True)
         n_graphs = unique.size(0)
         n_nodes = adj.shape[0]
 
         if self.normalize:
-            norm = counts.unsqueeze(1).repeat(1, self.n_subgraphs) 
+            norm = counts.unsqueeze(1).repeat(1, self.n_subgraphs)
 
         E = torch.ones((self.n_subgraphs, self.size_subgraphs, n_nodes), device=self.device)
 
@@ -58,17 +48,12 @@ class RW_GNN(nn.Module):
         random_walk_results = list()
 
         for i in range(1, self.max_step):
-
             I = torch.spmm(adj_power, I)
             P_power_E = torch.einsum("abc,acd->abd", (adj_sampled, P_power_E))
             res = torch.einsum("abc,cd->abd", (P_power_E, I))
-            res = torch.mul(E, res)
-            #the number of subgraph * the nodes number in subgraph * the number of nodes in this batch
-
             res = torch.zeros(res.size(0), res.size(1), n_graphs, device=self.device).index_add_(2, graph_indicator, res)
             res = torch.sum(res, dim=1)
             res = torch.transpose(res, 0, 1)
-
             if self.normalize:
                 res /= norm
             random_walk_results.append(res)
@@ -76,8 +61,5 @@ class RW_GNN(nn.Module):
 
         random_walk_results = torch.cat(random_walk_results, dim=1)
         random_walk_results = self.bn(random_walk_results)
-
         random_walk_results = self.layer_2(self.relu(self.layer_1(random_walk_results)))
-
-
         return random_walk_results
